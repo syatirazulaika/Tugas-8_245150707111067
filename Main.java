6314367cf802e5eb43fb66e25a7e77eb2e68a0d9
syatirazulaika <syatirazulaika@gmail.com>
2025-05-05 02:00:09 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,16 @@
-kk
+package PraktikumDelapan;
+
+public class Main {
+    public static void main(String[] args) {
+        Invoice[] invoices = new Invoice[] {
+            new Invoice("Beras", 2, 50000),
+            new Invoice("Minyak Goreng", 1, 30000),
+            new Invoice("Sabun", 3, 10000)
+        };
+
+        Employee emp = new Employee(101, "Syatira Zulaikanisa", 1000000, invoices);
+
+        emp.displayInfo();
+    }
+}
+
